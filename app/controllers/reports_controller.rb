0b041d762534c29c6fd1ab30ea69327c936d4464
Jose Antonio Torres Garibay <pepe_torres1998@hotmail.com>
2019-05-23 05:44:39 -0500

    Add reports scaffold

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,35 @@
+class ReportsController < ApplicationController
+  # before_action :set_report, only: [:show, :edit, :update, :destroy]
+
+  # GET /reports/new
+  def new
+    @report = Report.new
+  end
+
+  # POST /reports
+  # POST /reports.json
+  def create
+    @report = Report.new(report_params)
+
+    respond_to do |format|
+      if @report.save
+        format.html { redirect_to @report, notice: 'Report was successfully created.' }
+        format.json { render :show, status: :created, location: @report }
+      else
+        format.html { render :new }
+        format.json { render json: @report.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_report
+      @report = Report.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def report_params
+      params.require(:report).permit(:taximeter, :driving, :cleaning, :service)
+    end
+end
